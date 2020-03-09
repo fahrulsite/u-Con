77bd977b6dd65b778e1ee888e456c9332be382d8
fahrulsite <fahrulsite@gmail.com>
2020-03-09 20:25:01 +0700

    tambah layout detail

---
+++ app/src/main/java/com/fahrul/u_con/detailToko.kt
@@ -0,0 +1,12 @@
+package com.fahrul.u_con
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class detailToko : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_toko)
+    }
+}
